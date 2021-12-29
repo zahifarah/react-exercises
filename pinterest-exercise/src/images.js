5feeb59b11feb2a740af246908e386f6783f2bf7
Zahi Farah <zahi.farah@protonmail.com>
2021-12-29 13:47:23 +0200

    Basic set-up + create fake data-base + import Assets

---
+++ pinterest-exercise/src/images.js
@@ -0,0 +1,33 @@
+import Banner1 from "./Assets/banner-1.jpeg"
+import Banner2 from "./Assets/banner-2.jpeg"
+import Banner3 from "./Assets/banner-3.jpeg"
+import Banner4 from "./Assets/banner-4.jpeg"
+import Banner5 from "./Assets/banner-5.jpeg"
+import Banner6 from "./Assets/banner-6.jpeg"
+
+import Thumb1 from "./Assets/thumb-1.jpeg"
+import Thumb2 from "./Assets/thumb-2.jpeg"
+import Thumb3 from "./Assets/thumb-3.jpeg"
+import Thumb4 from "./Assets/thumb-4.jpeg"
+import Thumb5 from "./Assets/thumb-5.jpeg"
+import Thumb6 from "./Assets/thumb-6.jpeg"
+
+import Logo from "./Assets/AE_Logo.jpeg"
+
+const images = {
+  Logo,
+  Banner1,
+  Banner2,
+  Banner3,
+  Banner4,
+  Banner5,
+  Banner6,
+  Thumb1,
+  Thumb2,
+  Thumb3,
+  Thumb4,
+  Thumb5,
+  Thumb6,
+}
+
+export default images
---
+++ pinterest-exercise/src/index.js
@@ -0,0 +1,95 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import images from "./images"
+
+const Profile = ({ profileData }) => {
+  console.log(profileData)
+  return (
+    <>
+      <h2>Hello World!</h2>
+    </>
+  )
+}
+
+const aviationExplorer = {
+  headerData: {
+    heroTitle: "Aviation Explorer",
+    heroLogo: images.Logo,
+    statList: {
+      boards: 37,
+      pins: 8.9,
+      likes: 186,
+      followers: 8.9,
+      following: 1.8,
+    },
+  },
+
+  contentData: [
+    {
+      id: 1,
+      title: "Aviation 1",
+      subtitle: "Some subtitle 1",
+      banner: images.Banner1,
+      pins: 4850,
+      thumbnail_1: images.Thumb1,
+      thumbnail_2: images.Thumb1,
+      thumbnail_3: images.Thumb1,
+    },
+    {
+      id: 2,
+      title: "Aviation 2",
+      subtitle: "Some subtitle 2",
+      banner: images.Banner2,
+      pins: 641,
+      thumbnail_1: images.Thumb2,
+      thumbnail_2: images.Thumb2,
+      thumbnail_3: images.Thumb2,
+    },
+    {
+      id: 3,
+      title: "Aviation 3",
+      subtitle: "Some subtitle 3",
+      banner: images.Banner3,
+      pins: 209,
+      thumbnail_1: images.Thumb3,
+      thumbnail_2: images.Thumb3,
+      thumbnail_3: images.Thumb3,
+    },
+    {
+      id: 4,
+      title: "Aviation 4",
+      subtitle: "Some subtitle 4",
+      banner: images.Banner4,
+      pins: 1495,
+      thumbnail_1: images.Thumb4,
+      thumbnail_2: images.Thumb4,
+      thumbnail_3: images.Thumb4,
+    },
+    {
+      id: 5,
+      title: "Aviation 5",
+      subtitle: "Some subtitle 5",
+      banner: images.Banner5,
+      pins: 1495,
+      thumbnail_1: images.Thumb5,
+      thumbnail_2: images.Thumb5,
+      thumbnail_3: images.Thumb5,
+    },
+    {
+      id: 6,
+      title: "Aviation 6",
+      subtitle: "Some subtitle 6",
+      banner: images.Banner6,
+      pins: 75,
+      thumbnail_1: images.Thumb6,
+      thumbnail_2: images.Thumb6,
+      thumbnail_3: images.Thumb6,
+    },
+  ],
+}
+
+ReactDOM.render(
+  <Profile profileData={aviationExplorer} />,
+  document.querySelector("#root")
+)
