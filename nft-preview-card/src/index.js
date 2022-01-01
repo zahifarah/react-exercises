6ce24d37c3406b48b19defca22ad31701019da75
Zahi Farah <zahi.farah@protonmail.com>
2022-01-02 00:10:35 +0200

    HTML restructuring + styling

--- nft-preview-card/src/index.js
+++ nft-preview-card/src/index.js
@@ -1,14 +1,73 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import Artwork from "./Assets/image-equilibrium.jpg"
+import Avatar from "./Assets/image-avatar.png"
+import IconEthereum from "./Assets/icon-ethereum.svg"
+import IconClock from "./Assets/icon-clock.svg"
+
+import "./CSS/minireset.min.css"
 import "./CSS/styles.css"
 
-const NftPreview = () => {
+const NftPreview = ({ nfts }) => {
+  const {
+    id,
+    title,
+    artId,
+    creator,
+    description,
+    cost,
+    timeLeft,
+    artworkUrl,
+    avatarUrl,
+  } = nfts
   return (
-    <>
-      <h2>Hello World!</h2>
-    </>
+    <div className="container">
+      <div className="card__nft" key={id}>
+        <div className="artwork__container">
+          <img src={artworkUrl} alt="" className="artwork__image" />
+        </div>
+        <div className="artwork__info">
+          <div className="artwork__headers">
+            <span className="artwork__title">{title}</span>
+            <span className="artwork__id">#{artId}</span>
+          </div>
+          <p className="artwork__desc">{description}</p>
+          <div className="time-and-cost__container">
+            <div className="cost__container">
+              <img className="eth__icon" src={IconEthereum} alt="" />
+              <span className="cost">{cost} ETH</span>
+            </div>
+            <div className="time-left__container">
+              <img className="clock__icon" src={IconClock} alt="" />
+              <span className="time-left">{timeLeft}</span>
+            </div>
+          </div>
+          <div className="line"></div>
+          <div className="creator__info">
+            <span className="avatar__container">
+              <img className="avatar__image" src={avatarUrl} alt="" />
+            </span>
+            <span className="creation-of">
+              Creation of <span className="creator">{creator}</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-ReactDOM.render(<NftPreview />, document.querySelector("#root"))
+const nftInfo = {
+  id: 1,
+  title: "Equilibrium",
+  artId: 3429,
+  creator: "Jules Wyvern",
+  description: "Our Equilibrium collection promotes balance and calm.",
+  cost: 0.041,
+  timeLeft: "3 days left",
+  artworkUrl: Artwork,
+  avatarUrl: Avatar,
+}
+
+ReactDOM.render(<NftPreview nfts={nftInfo} />, document.querySelector("#root"))
