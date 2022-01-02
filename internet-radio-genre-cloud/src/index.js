67062ef0f2b0517f0707378eb7b98012eca9d0f7
Zahi Farah <zahi.farah@protonmail.com>
2022-01-02 19:16:00 +0200

    Setup basic fake database + link to css file + run component

--- internet-radio-genre-cloud/src/index.js
+++ internet-radio-genre-cloud/src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+
+import "./styles.css"
+
+const Panel = ({ genresList }) => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+const genresList = [
+  {
+    id: 1,
+    genre: "Smooth Jazz",
+  },
+  {
+    id: 2,
+    genre: "Christmas",
+  },
+  {
+    id: 3,
+    genre: "Classic Rock",
+  },
+  {
+    id: 4,
+    genre: "Blues",
+  },
+  {
+    id: 5,
+    genre: "Easy Listening",
+  },
+  {
+    id: 6,
+    genre: "Jazz",
+  },
+  {
+    id: 7,
+    genre: "Classical",
+  },
+  {
+    id: 8,
+    genre: "Country",
+  },
+  {
+    id: 9,
+    genre: "80s",
+  },
+  {
+    id: 10,
+    genre: "Rock",
+  },
+  {
+    id: 11,
+    genre: "Dance",
+  },
+  {
+    id: 12,
+    genre: "Oldies",
+  },
+  {
+    id: 13,
+    genre: "House",
+  },
+  {
+    id: 14,
+    genre: "Ambient",
+  },
+  {
+    id: 15,
+    genre: "Lounge",
+  },
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Panel genresInfo={genresList} />,
+  document.querySelector("#root")
+)
