123b98950106270782329086f70a687d53fc1ebb
Zahi Farah <zahi.farah@protonmail.com>
2022-03-07 23:19:40 +0200

    Basic tic-tac-toe functional

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -3,19 +3,54 @@ import ReactDOM from "react-dom"
 
 import "./styles.css"
 
-class Square extends React.Component {
-  render() {
-    return <button className="square">{this.props.value}</button>
-  }
+const Square = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
   renderSquare(i) {
-    return <Square value={i} />
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    console.log(calculateWinner(squares))
+    // Why is the second operand necessary?
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O"
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
   }
 
   render() {
-    const status = "Next player: X"
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next Player: ${this.state.xIsNext ? "X" : "O"}`
+    }
 
     return (
       <div>
@@ -57,3 +92,23 @@ class Game extends React.Component {
 }
 
 ReactDOM.render(<Game />, document.querySelector("#root"))
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
