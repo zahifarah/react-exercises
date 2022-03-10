7d50293e5550c017f06af1466f89ad76d718d67f
Zahi Farah <zahi.farah@protonmail.com>
2022-03-10 18:23:08 +0200

    Create Game component + basic HTML structure and styling

--- tic-tac-toe-personal/src/index.js
+++ tic-tac-toe-personal/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Game = () => {
+  return (
+    <>
+      <div className="board">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  )
+}
+
+ReactDOM.render(<Game />, document.querySelector("#root"))
