07a980012b43c8b3536436311aebaea8582db3f9
Zahi Farah <zahi.farah@protonmail.com>
2022-03-12 19:47:57 +0200

    Class based basic functionality.

--- tic-tac-toe-personal/src/index.js
+++ tic-tac-toe-personal/src/index.js
@@ -3,36 +3,68 @@ import ReactDOM from "react-dom"
 
 import "./styles.css"
 
-class Game extends Component {
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    )
+  }
+}
+
+class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext ? "X" : "O"
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+
   renderSquare(i) {
-    return <button className="square">{i}</button>
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
-    console.log(this.state)
+    return (
+      <div className="board">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
+      </div>
+    )
+  }
+}
+
+class Game extends Component {
+  render() {
     return (
       <>
         <div className="container">
-          <div className="status">Turn: X</div>
+          <div className="status">Next player: X</div>
           <div className="board-container">
-            <div className="board">
-              {this.renderSquare(0)}
-              {this.renderSquare(1)}
-              {this.renderSquare(2)}
-              {this.renderSquare(3)}
-              {this.renderSquare(4)}
-              {this.renderSquare(5)}
-              {this.renderSquare(6)}
-              {this.renderSquare(7)}
-              {this.renderSquare(8)}
-            </div>
+            <Board />
           </div>
         </div>
       </>
