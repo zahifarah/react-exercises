7c279de37a3aab883006ab6cd6b5580a13693fd5
Zahi Farah <zahi.farah@protonmail.com>
2022-03-12 15:22:06 +0200

    Trying to add a div above the board to mask the outer borders.

--- tic-tac-toe-personal/src/index.js
+++ tic-tac-toe-personal/src/index.js
@@ -8,16 +8,19 @@ const Game = () => {
     <>
       <div className="container">
         <div className="status">Turn: X</div>
-        <div className="board">
-          <button className="square">1</button>
-          <button className="square">2</button>
-          <button className="square">3</button>
-          <button className="square">4</button>
-          <button className="square">5</button>
-          <button className="square">6</button>
-          <button className="square">7</button>
-          <button className="square">8</button>
-          <button className="square">9</button>
+        <div className="board-container">
+          <div className="border-div"></div>
+          <div className="board">
+            <button className="square">1</button>
+            <button className="square">2</button>
+            <button className="square">3</button>
+            <button className="square">4</button>
+            <button className="square">5</button>
+            <button className="square">6</button>
+            <button className="square">7</button>
+            <button className="square">8</button>
+            <button className="square">9</button>
+          </div>
         </div>
       </div>
     </>
