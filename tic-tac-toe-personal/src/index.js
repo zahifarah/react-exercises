77badc0e70d814c5a14386b95731224ad741024a
Zahi Farah <zahi.farah@protonmail.com>
2022-03-14 12:48:14 +0200

    Add calculateWinner(squares) to bottom of code. Comment out. Test later

--- tic-tac-toe-personal/src/index.js
+++ tic-tac-toe-personal/src/index.js
@@ -73,3 +73,22 @@ class Game extends Component {
 }
 
 ReactDOM.render(<Game />, document.querySelector("#root"))
+
+// const calculateWinner = (squares) => {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ]
+//   const [a, b, c] = lines
+
+//   if ((squares[a] === squares[b]) === squares[c]) {
+//     return squares[a]
+//   }
+//   return null
+// }
