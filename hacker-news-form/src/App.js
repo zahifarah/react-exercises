89e085cefea05b45e3666b6c348a2b23b8a1dc40
Zahi Farah <zahi.farah@protonmail.com>
2022-03-20 14:18:48 +0200

    Add LoadingSpinner, search logic, autofocus input, some styling.

--- hacker-news-form/src/App.js
+++ hacker-news-form/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import LoadingSpinner from "./LoadingSpinner"
 
 import "./App.css"
 
@@ -8,13 +9,22 @@ const App = () => {
   const [url, setUrl] = useState(
     `https://hn.algolia.com/api/v1/search?query=${query}`
   )
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true)
       fetch(url)
         .then((res) => res.json())
-        .then((json) => setData(json))
-        .catch((error) => console.log(error.message))
+        .then((json) => {
+          setData(json)
+          setIsLoading(false)
+        })
+        .catch(() => {
+          setErrorMessage("Unable to fetch posts")
+          setIsLoading(false)
+        })
     }
 
     fetchData()
@@ -28,29 +38,36 @@ const App = () => {
 
   return (
     <div className="container">
-      {/* Search Query */}
-      <input
-        type="text"
-        onKeyDown={handleKeyDown}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-
-      {/* Search Command */}
-      <button
-        onClick={() =>
-          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
-        }>
-        Search
-      </button>
+      <div className="search-bar">
+        <input
+          autoFocus
+          type="text"
+          onKeyDown={handleKeyDown}
+          onChange={(e) => setQuery(e.target.value)}
+          className="search-input"
+        />
+        <button
+          onClick={() =>
+            setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+          }
+          disabled={isLoading}
+          className="search-button">
+          Search
+        </button>
+      </div>
 
       {/* Display Posts */}
-      <ul className="list">
-        {data.hits.map((item) => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <ul>
+          {data.hits.map((item) => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
