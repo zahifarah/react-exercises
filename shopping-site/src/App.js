1a3e95688ea4540861950dcdb9ad6b788a200808
Zahi Farah <zahi.farah@protonmail.com>
2022-04-03 18:41:06 +0300

    Setup project

---
+++ shopping-site/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <>
+      <div>Shopping Site</div>
+    </>
+  )
+}
+
+export default App
---
+++ shopping-site/src/index.js
@@ -0,0 +1,7 @@
+import * as ReactDOMClient from "react-dom/client"
+import App from "./App"
+
+const container = document.getElementById("app")
+const root = ReactDOMClient.createRoot(container)
+
+root.render(<App />)
