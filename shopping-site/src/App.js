dfc080f9b51c087179777b6f0cddd34dd5a2ac5f
Zahi Farah <zahi.farah@protonmail.com>
2022-04-03 20:13:18 +0300

    Render Items component using ItemPage and Item components. Styling done as well.

--- shopping-site/src/App.js
+++ shopping-site/src/App.js
@@ -1,31 +1,36 @@
 import React, { useState } from "react"
 import Nav from "./Nav"
 import ItemPage from "./ItemPage"
-import { items } from "./static-data"
 
+import { items } from "./static-data"
 import "./App.css"
+
+const Content = ({ tab, onAddToCart }) => {
+  switch (tab) {
+    default:
+    case "items":
+      return <ItemPage items={items} onAddToCart={onAddToCart} />
+    case "cart":
+      return <span>render cart for case "cart"</span>
+  }
+}
 
 const App = () => {
   const [activeTab, setActiveTab] = useState("items")
+  const [cart, setCart] = useState([])
+
+  const addToCart = (item) => {
+    setCart((prevCart) => [...prevCart, item])
+  }
 
   return (
     <div className="App">
       <Nav activeTab={activeTab} onTabChange={setActiveTab} />
       <main className="App-content">
-        <Content tab={activeTab} />
+        <Content tab={activeTab} onAddToCart={addToCart} />
       </main>
     </div>
   )
-}
-
-const Content = ({ tab }) => {
-  switch (tab) {
-    default:
-    case "items":
-      return <ItemPage items={items} />
-    case "cart":
-      return <span>render cart for case "cart"</span>
-  }
 }
 
 export default App
