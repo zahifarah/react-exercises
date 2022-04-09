bc63059fd836f65b863f911b00e192cca995a1cc
Zahi Farah <zahi.farah@protonmail.com>
2022-04-09 21:32:18 +0300

    Connect css files, import google fonts, more css resets, implement dark theme

--- reddit-exercise/src/index.js
+++ reddit-exercise/src/index.js
@@ -3,6 +3,8 @@ import ReactDOMClient from "react-dom/client"
 
 import App from "./App"
 
+import "./index.css"
+
 const container = document.getElementById("app")
 const root = ReactDOMClient.createRoot(container)
 
