727e892cd6f64eff8d078cae25654699c7e9e6fa
Zahi Farah <zahi.farah@protonmail.com>
2022-04-09 22:21:38 +0300

    CSS: post > header-container

--- reddit-exercise/src/App.js
+++ reddit-exercise/src/App.js
@@ -4,35 +4,42 @@ import "./App.css"
 
 export default function App() {
   return (
-    <div className="wrapper">
-      <div className="app-container">
-        <div className="post">
-          <div className="header-container">
-            <div className="thumbnail">IMG</div>
-            <div className="information">Posted by Vayl 10 hours ago</div>
+    <div className="app-container">
+      <div className="post">
+        <div className="header-container">
+          <div className="thumbnail-container">
+            <img
+              class="thumbnail"
+              alt="Subreddit Icon"
+              src="https://styles.redditmedia.com/t5_2qkaf/styles/communityIcon_s2cfjhxnjqq61.png"></img>
           </div>
-          <div className="title-container">
-            <h2 className="title">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisiiure
-            </h2>
-          </div>
-          <div className="body-container">
-            <div className="content-mask"></div>
-            <div className="content">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-              placeat cumque illum ducimus laudantium mollitia minima numquam
-              temporibus odit totam ratione, itaque ut cupiditate quis, unde
-              magnam, dolorum consequatur velit! Quia nemo animi debitis ipsa
-              consectetur dolore, nulla voluptatibus vero dolorum quis eveniet
-              aliquid exercitationem omnis mollitia eligendi, iusto accusamus,
-              tempora reprehenderit? Quidem voluptate iusto beatae in reiciendis
-              fugit dolore.
+          <div className="information-container">
+            <div className="subreddit">r/networking</div>
+            <div className="information">
+              . Posted by Vayl 10 hours ago lorem
             </div>
-            <div className="post-option">
-              <div className="arrows">22</div>
-              <div className="options">
-                <span>50 comments</span>
-              </div>
+          </div>
+        </div>
+        <div className="title-container">
+          <h2 className="title">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisiiure
+          </h2>
+        </div>
+        <div className="body-container">
+          <div className="content-mask"></div>
+          <div className="content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut placeat
+            cumque illum ducimus laudantium mollitia minima numquam temporibus
+            odit totam ratione, itaque ut cupiditate quis, unde magnam, dolorum
+            consequatur velit! Quia nemo animi debitis ipsa consectetur dolore,
+            nulla voluptatibus vero dolorum quis eveniet aliquid exercitationem
+            omnis mollitia eligendi, iusto accusamus, tempora reprehenderit?
+            Quidem voluptate iusto beatae in reiciendis fugit dolore.
+          </div>
+          <div className="post-option">
+            <div className="arrows">22</div>
+            <div className="options">
+              <span>50 comments</span>
             </div>
           </div>
         </div>
