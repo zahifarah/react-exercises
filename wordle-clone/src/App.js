da10175e1168a3c60f8a4ead4eb092f2d27fff24
Zahi Farah <zahi.farah@protonmail.com>
2022-04-09 13:51:54 +0300

    Delete wordle-clone directory

--- wordle-clone/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import "./App.css"
-
-const App = () => {
-  return <div>Hello World</div>
-}
-
-export default App
--- wordle-clone/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOMClient from "react-dom/client"
-
-import App from "./App"
-
-const container = document.getElementById("app")
-const root = ReactDOMClient.createRoot(container)
-
-root.render(<App />)
