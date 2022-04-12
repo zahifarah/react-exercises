31f5d9eee388d7c583358fd223361ca239347ba0
Zahi Farah <zahi.farah@protonmail.com>
2022-04-12 14:46:22 +0300

    Basic structure done over

--- reddit-exercise/src/App.js
+++ reddit-exercise/src/App.js
@@ -13,9 +13,66 @@ export default function App() {
             <div className="reputation-ele">DW</div>
           </div>
           <div className="thumbnail-container">
-            <div className="thumbnail"></div>
+            <img
+              src="https://b.thumbs.redditmedia.com/eLMKVL5FFpkQqzB-QsQcaBX14Kwb81BiW5vNz_0N4is.jpg"
+              alt=""
+              className="thumbnail"
+            />
           </div>
-          <div className="information"></div>
+          <div className="information-wrapper">
+            <div className="information-container">
+              <div className="title">
+                Big News Today as Everyone in Garden State Dies of Having Eaten
+                too many of dem apples m8
+                <span className="source">(netflix.com)</span>
+              </div>
+              <div className="submitted-by">
+                Submitted by Vaylx 10 hours ago
+              </div>
+              <div className="options-container">
+                <div className="options-comments">comments</div>
+                <div className="options-menu">
+                  <div>save</div>
+                  <div>share</div>
+                  <div>report</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="post">
+          <div className="reputation">
+            <div className="reputation-ele">UP</div>
+            <div className="reputation-ele">22</div>
+            <div className="reputation-ele">DW</div>
+          </div>
+          <div className="thumbnail-container">
+            <img
+              src="https://b.thumbs.redditmedia.com/eLMKVL5FFpkQqzB-QsQcaBX14Kwb81BiW5vNz_0N4is.jpg"
+              alt=""
+              className="thumbnail"
+            />
+          </div>
+          <div className="information-wrapper">
+            <div className="information-container">
+              <div className="title">
+                Big News Today as Everyone in Garden State Dies of Having Eaten
+                too many of dem apples m8
+                <span className="source">(netflix.com)</span>
+              </div>
+              <div className="submitted-by">
+                Submitted by Vaylx 10 hours ago
+              </div>
+              <div className="options-container">
+                <div className="options-comments">comments</div>
+                <div className="options-menu">
+                  <div>save</div>
+                  <div>share</div>
+                  <div>report</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
