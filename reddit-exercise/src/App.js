21a54752a4e70f183a3329141ee1d5fd3c1ad01a
Zahi Farah <zahi.farah@protonmail.com>
2022-04-12 15:04:41 +0300

    General layout and styling changes

--- reddit-exercise/src/App.js
+++ reddit-exercise/src/App.js
@@ -7,44 +7,12 @@ export default function App() {
     <div className="app-wrapper">
       <div className="app-container">
         <div className="post">
-          <div className="reputation">
-            <div className="reputation-ele">&#8593;</div>
-            <div className="reputation-ele">22</div>
-            <div className="reputation-ele">&#8595;</div>
-          </div>
-          <div className="thumbnail-container">
-            <img
-              src="https://b.thumbs.redditmedia.com/eLMKVL5FFpkQqzB-QsQcaBX14Kwb81BiW5vNz_0N4is.jpg"
-              alt=""
-              className="thumbnail"
-            />
-          </div>
-          <div className="information-wrapper">
-            <div className="information-container">
-              <div className="title">
-                Big News Today as Everyone in Garden State Dies of Having Eaten
-                too many of dem apples m8
-                <span className="source">(netflix.com)</span>
-              </div>
-              <div className="submitted-by">
-                Submitted by Vaylx 10 hours ago
-              </div>
-              <div className="options-container">
-                <div className="options-comments">comments</div>
-                <div className="options-menu">
-                  <div>save</div>
-                  <div>share</div>
-                  <div>report</div>
-                </div>
-              </div>
+          <div className="reputation-wrapper">
+            <div className="reputation-container">
+              <div className="reputation-ele">&#8593;</div>
+              <div className="reputation-ele">22</div>
+              <div className="reputation-ele">&#8595;</div>
             </div>
-          </div>
-        </div>
-        <div className="post">
-          <div className="reputation">
-            <div className="reputation-ele">&#8593;</div>
-            <div className="reputation-ele">22</div>
-            <div className="reputation-ele">&#8595;</div>
           </div>
           <div className="thumbnail-container">
             <img
